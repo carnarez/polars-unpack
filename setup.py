c3d3fafa5c0862d190d15db8b9b611235852a686
Clément Arnarez <11967125+carnarez@users.noreply.github.com>
2023-07-07 13:40:05 +0200

    Tweaks (#8)
    * Stored the samples away
    * Added the polars Python lib as requirement

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import setuptools
 setuptools.setup(
     author="carnarez",
     description=("Automated, schema-based JSON unpacking to Polars objects."),
+    install_requires=["polars"],
     name="polars_unpack",
     packages=["polars_unpack"],
     package_data={"polars_unpack": ["py.typed"]},
